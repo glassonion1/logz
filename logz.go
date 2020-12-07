cb757ff5a2215432f18a885b14fc03809483420d
taisuke.fujita <glassonion999@gmail.com>
2020-12-07 10:36:22 +0900

    Add some comments

--- logz.go
+++ logz.go
@@ -1,3 +1,9 @@
+/*
+Package logz provides the structured log.
+Example:
+	ctx := r.Context() // r is *http.Request
+	logz.Infof(ctx, "info log. requestURL: %s", r.URL.String())
+*/
 package logz
 
 import (
@@ -9,22 +15,27 @@ import (
 
 var std = &logger.Logger{}
 
+// Debugf writes debug log to the stdout
 func Debugf(ctx context.Context, format string, a ...interface{}) {
 	std.WriteLog(ctx, severity.Default, format, a...)
 }
 
+// Infof writes info log to the stdout
 func Infof(ctx context.Context, format string, a ...interface{}) {
 	std.WriteLog(ctx, severity.Info, format, a...)
 }
 
+// Warningf writes warning log to the stdout
 func Warningf(ctx context.Context, format string, a ...interface{}) {
 	std.WriteLog(ctx, severity.Warning, format, a...)
 }
 
+// Errorf writes error log to the stdout
 func Errorf(ctx context.Context, format string, a ...interface{}) {
 	std.WriteLog(ctx, severity.Error, format, a...)
 }
 
+// Criticalf writes critical log to the stdout
 func Criticalf(ctx context.Context, format string, a ...interface{}) {
 	std.WriteLog(ctx, severity.Critical, format, a...)
 }
