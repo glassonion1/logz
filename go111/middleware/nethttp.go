d380c7a84383205037b742b611b92e6a787e5805
taisuke.fujita <glassonion999@gmail.com>
2020-12-14 21:14:34 +0900

    Fix bug of middleware on go111

--- go111/middleware/nethttp.go
+++ go111/middleware/nethttp.go
@@ -12,16 +12,16 @@ func NetHTTP(label string) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-			span := &trace.Span{}
-			defer span.End()
-
 			ctx := r.Context()
+			var span *trace.Span
+
 			hf := propagation.HTTPFormat{}
 			if sc, ok := hf.SpanContextFromRequest(r); ok {
 				ctx, span = trace.StartSpanWithRemoteParent(ctx, label, sc)
 			} else {
 				ctx, span = trace.StartSpan(ctx, label)
 			}
+			defer span.End()
 
 			h.ServeHTTP(w, r.WithContext(ctx))
 		})
