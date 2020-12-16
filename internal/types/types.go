844ef2b968bc3e1095dcac368efae056201f3a9b
taisuke.fujita <glassonion999@gmail.com>
2020-12-16 19:45:29 +0900

    fix access log

--- internal/types/types.go
+++ internal/types/types.go
@@ -25,6 +25,8 @@ type AccessLog struct {
 	Time        time.Time   `json:"time"`
 	Trace       string      `json:"logging.googleapis.com/trace"`
 	HTTPRequest HTTPRequest `json:"httpRequest"`
+	JSONPayload interface{} `json:"jsonPayload"`
+	Message     string      `json:"message"`
 }
 
 type SourceLocation struct {
