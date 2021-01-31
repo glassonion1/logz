123809d086a03930a90cb0d74015abf4273c9f86
taisuke.fujita <glassonion999@gmail.com>
2021-01-31 14:37:30 +0900

    Add a example code

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package logz_test
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/glassonion1/logz"
 	"github.com/glassonion1/logz/middleware"
@@ -25,7 +26,7 @@ func Example() {
 	log.Fatal(http.ListenAndServe(":8080", h))
 }
 
-func ExampleConfig() {
+func ExampleSetConfig() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
@@ -37,6 +38,33 @@ func ExampleConfig() {
 	logz.SetConfig(logz.Config{
 		ProjectID:      "your gcp project id",
 		NeedsAccessLog: false, // Whether or not to write the access log
+	})
+	logz.InitTracer()
+	// Sets the middleware
+	h := middleware.NetHTTP("tracer name")(mux)
+
+	log.Fatal(http.ListenAndServe(":8080", h))
+}
+
+func ExampleSetConfig_changeLogout() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		// Writes info log
+		logz.Infof(ctx, "writes %s log", "info")
+	})
+
+	// Writes log on local file
+	file, err := os.OpenFile("local.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		panic(err)
+	}
+	logz.SetConfig(logz.Config{
+		ProjectID:         "your gcp project id",
+		NeedsAccessLog:    false,
+		AccessLogOut:      file,
+		ApplicationLogOut: file,
 	})
 	logz.InitTracer()
 	// Sets the middleware
